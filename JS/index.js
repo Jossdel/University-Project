ccd2fd415573a00fc574c58714b7f45bc2102e67
Jossdel <168942707+Jossdel@users.noreply.github.com>
2026-03-22 17:57:16 -0400

    modificacion de navbar

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,8 @@
-window.addEventListener("scroll", function () {
-  let navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 
+window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     navbar.style.background = "rgba(0,0,0,0.7)";
-    navbar.style.transition = "0.9s";
   } else {
     navbar.style.background = "rgba(0,0,0,0.7)";
   }
