b168dc55ec20d9ea73882bc87df75a569a4792b3
Jossdel <168942707+Jossdel@users.noreply.github.com>
2026-03-29 18:06:29 -0400

    updates

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -1,9 +1,1 @@
 const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.style.background = "rgba(0,0,0,0.7)";
-  } else {
-    navbar.style.background = "rgba(0,0,0,0.7)";
-  }
-});
